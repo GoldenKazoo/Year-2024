40eb3693211d52bf0211bfb99142875c91eb79b0
GoldenKazoo <125774398+GoldenKazoo@users.noreply.github.com>
2024-01-01 23:56:11 +0100

    Day 1 2024

---
+++ Day1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I like pizza !\n");
+    printf("It's really good \n");
+    return 0;
+
+    int x; //declaration d'un entier
+    x = 1; //initialisation d'un entier
+    int x = 1; //declaration + initialisation
+
+    float x = 1.0; //declaration + initialisation d'un entier 
+
+    char x = 'c'; //declaration + initialisation d'un caractere 
+
+    char x[]  = "Mot"; //declaration + initialisation d'un tableau de caracteres
+
+
+}
