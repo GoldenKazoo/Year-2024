6ed4baa123a5ce6ed93e29c71371c5798723893f
GoldenKazoo <125774398+GoldenKazoo@users.noreply.github.com>
2024-01-09 22:59:34 +0100

    Day 9

---
+++ Day9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+
+    double x = sqrt(2);
+    double y = pow(2,4);
+    double y = round(3.14);
+    double y = ceil(3.14);
+    double y = floor(3.99);
+    double y = fabs(-100);
+    double y = log (3);
+    double y = sin(45);
+    double y = cos(45);
+    double y = tan(45);
+
+}
